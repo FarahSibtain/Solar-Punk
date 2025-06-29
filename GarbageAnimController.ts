e2e8390b1353cee82845d8a5eb56a173baf08665
FarahXR <ATBB9vY5WVvxX6es8uDTgbTx4AVc4D6DC0A2>
2025-06-29 08:05:36 +0200

    float object

--- GarbageAnimController.ts
+++ GarbageAnimController.ts
@@ -1,6 +1,7 @@
 import { Component, Behavior, BehaviorConstructorProps, ContextManager, registerBehaviorRunAtDesignTime, started } from "@zcomponent/core";
 import { Group as Group } from "@zcomponent/three/lib/components/Group";
 import Scene from "./Scene.zcomp";
+import Box from "./GrabbableFlop.zcomp";
 
 interface ConstructionProps {
 	// Add any constructor props you'd like for your behavior here
@@ -13,7 +14,7 @@ interface ConstructionProps {
 export class GarbageAnimController extends Behavior<Group> {
 
 	protected zcomponent = this.getZComponentInstance(Scene);
-		
+	
 
 	constructor(contextManager: ContextManager, instance: Group, protected constructorProps: ConstructionProps) {
 		super(contextManager, instance);
@@ -26,6 +27,21 @@ export class GarbageAnimController extends Behavior<Group> {
 			// Start the GarbagePath animation (Collection movement)
 			this.zcomponent.animation.layers.GarbagePath.clips.GP.play({ loop: true });			
 		});
+
+		// Get the Box component instance using entityByID
+		const boxEntity = this.zcomponent.entityByID.get('c142570ec5ec46a0a190e6faa99ea252') as Box;
+		
+		if (boxEntity && boxEntity.nodes) {
+			// Stop wobble animation when object is grabbed  
+			this.register(boxEntity.nodes.RigidbodyEventEmitter.onGrab, () => {
+				this.zcomponent.animation.layers.Wobble.clips.Wobble0.pause();
+			});
+
+			// Resume wobble animation when object is released
+			this.register(boxEntity.nodes.RigidbodyEventEmitter.onRelease, () => {
+				this.zcomponent.animation.layers.Wobble.clips.Wobble0.play({ loop: true });
+			});
+		}
 
 		/*
 		// You can register handlers for events on the node that this behavior
